609b1e5006cebd8d7f5787be49f386178e920ab7
William Chang (CI) <wilicw1346@gmail.com>
2018-12-01 12:03:35 +0000

    deploy

---
+++ precache-manifest.dcc17fac0d4f20f7afdce18016733bff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4234698c5de72d3c7696",
+    "url": "/resicolor/css/app.d46880a9.css"
+  },
+  {
+    "revision": "4234698c5de72d3c7696",
+    "url": "/resicolor/js/app.d1ebfef1.js"
+  },
+  {
+    "revision": "5eab3ef3b078dbdc1a73",
+    "url": "/resicolor/css/chunk-vendors.9c8e3772.css"
+  },
+  {
+    "revision": "5eab3ef3b078dbdc1a73",
+    "url": "/resicolor/js/chunk-vendors.3e1be7bf.js"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/resicolor/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/resicolor/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "ecb0c2ae369ba2a89d9a1ec2a1b3187b",
+    "url": "/resicolor/fonts/AvenirLTStd-Book.ecb0c2ae.otf"
+  },
+  {
+    "revision": "04907632dc24ff4de0fa9bbcf30cdec7",
+    "url": "/resicolor/index.html"
+  },
+  {
+    "revision": "bd22d29a24aa685cfaea0796c72d0ee0",
+    "url": "/resicolor/icon.png"
+  },
+  {
+    "revision": "a29c2b90494507f2a2f8bb060550199f",
+    "url": "/resicolor/robots.txt"
+  },
+  {
+    "revision": "904509ab09783047eec5e9f4e9c91d29",
+    "url": "/resicolor/sitemap.xml"
+  },
+  {
+    "revision": "832669588daa213185a4b2703fe898cd",
+    "url": "/resicolor/screen0.png"
+  },
+  {
+    "revision": "2aa1cccd1cffdfcdf23c28fa8dd6ea05",
+    "url": "/resicolor/screen1.png"
+  }
+];
